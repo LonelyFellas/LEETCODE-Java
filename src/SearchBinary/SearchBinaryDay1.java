e7baca936cc869300f65614cc4dc02482afd93c5
LonelyFellas <289582304@qq.com>
2023-06-25 20:18:18 +0800

    Update: 二分法，Leetcode No.704

--- src/SearchBinary/SearchBinaryDay1.java
+++ src/SearchBinary/SearchBinaryDay1.java
@@ -10,6 +10,30 @@ public class SearchBinaryDay1 {
     }
 
     /**
+     * Leetcode No.704 位置查找
+     * @param nums 给定的数组
+     * @param target 搜索的目标
+     * @return 返回目标第一个下标
+     */
+    public int search(int[] nums, int target) {
+       int left = 0;
+       int right = nums.length - 1;
+
+       while (left <= right) {
+           int mid = (left + right) / 2;
+           if (nums[mid] == target) {
+               return mid;
+           } else if (nums[mid] < target) {
+               left = mid + 1;
+           } else {
+               right = mid - 1;
+           }
+       }
+
+       return -1;
+    }
+
+    /**
      * Leetcode No.2089 找出数据排序后的目标下标
      *
      * @param nums 给定的数组
