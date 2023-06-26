a6981b49ef1d9320fe5cbbacab661e9a58cf57ab
LonelyFellas <289582304@qq.com>
2023-06-26 22:33:03 +0800

    Update: leetcode No.2389

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import SearchBinary.Day2;
 import SearchBinary.SearchBinaryDay1;
 
 import java.util.Arrays;
@@ -5,9 +6,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        SearchBinaryDay1 searchBinaryDay1 = new SearchBinaryDay1();
+//        SearchBinaryDay1 searchBinaryDay1 = new SearchBinaryDay1();
 //        List<Integer> result = searchBinaryDay1.targetIndices(new int[] {1,2,5,2,3}, 5);
-        int result = searchBinaryDay1.search(new int[] {-1,0,3,5,9,12}, -1, false);
+//        int result = searchBinaryDay1.search(new int[] {-1,0,3,5,9,12}, -1, false);
+        Day2 day2 = new Day2();
+        int[] result = day2.answerQueries(new int[] {2, 3, 4, 5}, new int[] {1});
         System.out.println(result);
     }
 }
