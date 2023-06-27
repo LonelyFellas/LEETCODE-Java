9c4e8e10e83e579161fb27a419d86b58ef7f8173
LonelyFellas <289582304@qq.com>
2023-06-27 20:59:43 +0800

    Update: leetcode No.剑指 Offer II 069. 山峰数组的顶部

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,8 @@ public class Main {
 //        List<Integer> result = searchBinaryDay1.targetIndices(new int[] {1,2,5,2,3}, 5);
 //        int result = searchBinaryDay1.search(new int[] {-1,0,3,5,9,12}, -1, false);
         Day2 day2 = new Day2();
-        int[] result = day2.answerQueries(new int[] {2, 3, 4, 5}, new int[] {1});
+//        int[] result = day2.answerQueries(new int[] {2, 3, 4, 5}, new int[] {1});
+        int result = day2.peekIndexInMountainArrayBySearch(new int[] {24,69,100,99,79,78,67,36,26,19});
         System.out.println(result);
     }
 }
--- src/SearchBinary/Day2.java
+++ src/SearchBinary/Day2.java
@@ -10,6 +10,61 @@ public class Day2 {
     }
 
     /**
+     * Leetcode NO.剑指Office II 069 山峰数组的顶端 -
+     * 二分搜索法
+     * @param arr 山峰的数据
+     * @return 山峰的顶峰的下标
+     */
+    public int peekIndexInMountainArrayBySearch(int[] arr) {
+       int i = 0;
+       int j = arr.length - 1;
+
+       while(i < j) {
+           int mid = (i + j) / 2;
+           if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+               i = mid;
+               break;
+           } if (arr[mid] < arr[mid + 1]) {
+               i = mid;
+           } else if (arr[mid] < arr[mid - 1]) {
+               j = mid;
+           }
+       }
+
+       return i;
+    }
+
+    /**
+     * Leetcode NO.剑指Office II 069 山峰数组的顶端 -
+     * 双指针的解法
+     * @param arr 山峰的数据
+     * @return 山峰的顶峰的下标
+     */
+    public int peakIndexInMountainArray(int[] arr) {
+        int len = arr.length;
+        int i = 0;
+        int j = len - 1;
+        int lastI = -1;
+        int lastJ = -1;
+        int top = -1;
+        while(i <= j) {
+            if (arr[i] <= lastI || arr[j] <= lastJ) {
+                top = arr[i] <= lastI ? i - 1 : j + 1;
+                break;
+            }
+            lastI = arr[i];
+            lastJ = arr[j];
+            i++;
+            j--;
+        }
+        if (top == -1) {
+            int mid = len / 2;
+            top = arr[mid - 1] < arr[mid] ? mid : mid - 1;
+        }
+
+        return top;
+    }
+    /**
      * Leetcode No.2389 和有限的最长子序列
      * @param nums
      * @param queries
